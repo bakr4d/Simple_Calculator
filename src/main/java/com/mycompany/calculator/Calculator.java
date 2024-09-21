77fb46bc032db494d2a0d822abb74f325f384056
Bakr <mbakr9212@gmail.com>
2024-09-21 15:05:55 +0300

    Simple Calculator

---
+++ src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lenovo
+ */
+
+public class Calculator  {
+    Calculator(){
+        
+    }
+
+    public static void main(String[] args) {
+              
+        
+    }
+    
+}
+//  Scanner input = new Scanner (System.in);
+//        Double num1,num2,sum;
+//        System.out.println("Enter The first number Please");
+//        num1=input.nextDouble();
+//         System.out.println("Enter The Second number Please");
+//         num2=input.nextDouble();
+//         sum=num1+num2;
+//
+//         System.out.println("The Sum Is :" + sum);
+//         
